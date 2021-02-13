0ecb552724b985b8f604247de30708d13944c88c
Will Worthington <willworthington99@yahoo.com>
2021-02-13 16:51:33 -0600

    First attempt at fair bathroom

--- hw2/FairUnifanBathroom.java
+++ hw2/FairUnifanBathroom.java
@@ -1,22 +1,53 @@
-// EID 1
-// EID 2
+// wjw692
+// zbt86
 
 public class FairUnifanBathroom {   
+	private int utCnt;
+	private int ouCnt;
+	//Queue<>
 	
-  public synchronized void enterBathroomUT() {
+	public FairUnifanBathroom() {
+		utCnt = 0;
+		ouCnt = 0;
+	}
+	
+	public synchronized void enterBathroomUT() {
     // Called when a UT fan wants to enter bathroom	
-  }
+		while(ouCnt>0 || utCnt>=4) {
+			// Then at least one ou fan is in bathroom -> need to wait
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		utCnt++;
+	}
 	
 	public synchronized void enterBathroomOU() {
     // Called when a OU fan wants to enter bathroom
+		while(utCnt>0 || ouCnt>=4) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		ouCnt++;
 	}
 	
 	public synchronized void leaveBathroomUT() {
     // Called when a UT fan wants to leave bathroom
+		utCnt--;
+		notifyAll();
 	}
 
 	public synchronized void leaveBathroomOU() {
     // Called when a OU fan wants to leave bathroom
+		ouCnt--;
+		notifyAll();
 	}
 }
 	
