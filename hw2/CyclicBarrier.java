3e70805d48d4aa91979afe8a2862ceb968ddffd5
Zander Tedjo <zander.b.tedjo@gmail.com>
2021-02-24 23:39:11 -0600

    Second attempt at cyclic barrier. Only uses 1 sema. Better

--- hw2/CyclicBarrier.java
+++ hw2/CyclicBarrier.java
@@ -1,18 +1,52 @@
-/*
- * EID's of group members
- * 
- */
+// EID 1 = wjw692
+// EID 2 = zbt86
+
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
 
 public class CyclicBarrier {
-	
+	int parties;
+	int count;
+	CountingSemaphore countingSema;			// count threads waiting at gate
+	CountingSemaphore gateSema;				// block threads, then release all when appropriate
+
+	// count n while gate is closed
+	// close counter
+	// release n from gate
+	// close gate
+	// open counter
+
+
 	public CyclicBarrier(int parties) {
+		this.parties = parties;
+		this.count = 0;
+		gateSema = new CountingSemaphore(0);
 	}
 	
 	public int await() throws InterruptedException {
-           int index = 0;
-		
-          // you need to write this code
+		int index = parties-1-count;
+		count++;
+
+		// last thread opens gate for n threads
+		if(count == parties){
+			for(int i=0; i<parties; i++){
+				gateSema.V();
+			}
+		}
+
+		// stop or pass at gate
+		System.out.println("Stop at gate");
+		gateSema.P();
+		System.out.println("Pass gate");
+
+		// last thread resets barrier
+		if(count == parties) {
+			// close gate
+			gateSema.setValue(0);
+
+			// reset count
+			count = 0;
+		}
+
 	    return index;
 	}
 }
@@ -21,21 +55,30 @@ public class CyclicBarrier {
 
 class CountingSemaphore {
 	int value;
+
 	public CountingSemaphore(int initValue) {
 		value = initValue;
 	}
 	public synchronized void P() throws InterruptedException {
 		while (value == 0)
 			try{
-				this.wait();
+				wait();
 			} catch(Exception e){
 				throw e;
 			}
 		value--;
-
 	}
 	public synchronized void V() {
 		value++;
 		notify();
+	}
+	public synchronized int getValue(){
+		return value;
+	}
+	public synchronized void setValue(int val){
+		value = val;
+	}
+	public synchronized void openAll(){
+		notifyAll();
 	}
 }
